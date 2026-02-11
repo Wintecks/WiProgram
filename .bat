2a1516e0691a1535e24586356434a425b99e24b7
Wintecks <wintecks78@gmail.com>
2026-02-11 21:59:21 +0200

    Need

--- .bat
+++ .bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 pyinstaller -w -F --name "WiProgram" --icon=NONE main.py
-=======
-pyinstaller -w -F --name "WiProgram" --icon=NONE main.py
->>>>>>> 9f8c80cd2bc46e8034925d1f8f7512c945574f0b
